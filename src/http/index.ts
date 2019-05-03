90acdb42afcbe6f5fee570b67a6e2691f9bd46d6
rmchen-12 <rmchen1993@163.com>
2019-05-03 20:28:19 +0800

    修改请求端口

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,8 +1,8 @@
-import Axios from "axios";
+import Axios from 'axios';
 
 export const baseURL =
   process.env.NODE_ENV === "production"
-    ? "http://47.99.146.10:8080"
+    ? "http://localhost:7000"
     : "http://localhost:8080";
 
 export const http = Axios.create({
