ff9e7d170a0e1fd14c06d7d9552138ef62e5479f
rmchen-12 <rmchen1993@163.com>
2019-05-16 22:06:10 +0800

    修改背景色

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import * as loadable from "react-loadable";
-import registerServiceWorker from "./registerServiceWorker";
-import frontLoading from "./components/Loading/frontLoading";
-import adminLoading from "./components/Loading/adminLoading";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as loadable from 'react-loadable';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import adminLoading from './components/Loading/adminLoading';
+import frontLoading from './components/Loading/frontLoading';
+import registerServiceWorker from './registerServiceWorker';
 
 const Front = loadable({
   loader: () => import("./pages/front/index"),
