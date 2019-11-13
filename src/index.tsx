63efda668ccb8b4df1117e6dab0f76063f160548
ruiminChen <ruimin.chen@zuolin.com>
2019-11-13 19:03:53 +0800

    https测试

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as loadable from "react-loadable";
+import { FastClick } from "fastclick";
 
 import frontLoading from "./components/Loading/frontLoading";
 import registerServiceWorker from "./registerServiceWorker";
+
+FastClick.attach(document.body);
 
 const Front = loadable({
   loader: () => import("./pages/front/index"),
